69db3d0624530df13f20d26306566a3a874bb2c9
karakilicustasi <yildirim.dasdemir@ceng.deu.edu.tr>
2019-02-07 07:25:03 +0300

    calculation function v0

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -13,8 +13,10 @@ public class Game : MonoBehaviour {
     static Text button5;
     static Text button6;
     public Text[] buttons= { button1, button2, button3, button4, button5, button6};
-    Boolean permission=true;
-    Text mainButton;//Ulaşmak istediğimiz sayı
+    bool permission=false;
+    static Text mainButton;//Ulaşmak istediğimiz sayı
+    string calculation = "";
+    double sum = 0;
     
     private string mainString="";
 
@@ -24,11 +26,60 @@ public class Game : MonoBehaviour {
     }
     public void ButtonPressed()
     {
-        Debug.Log(buttons[Int32.Parse(EventSystem.current.currentSelectedGameObject.tag)-1].text);
+        //buttons[Int32.Parse(EventSystem.current.currentSelectedGameObject.tag) - 1].text
+        if (EventSystem.current.currentSelectedGameObject.name == "+" || EventSystem.current.currentSelectedGameObject.name == "-" || EventSystem.current.currentSelectedGameObject.name == "x" || EventSystem.current.currentSelectedGameObject.name == "div")
+        {
+           // Debug.Log("hey");
+            if (permission)
+            {
+                if(EventSystem.current.currentSelectedGameObject.name != "div")
+                    mainString += " " + EventSystem.current.currentSelectedGameObject.tag;
+                else
+                    mainString += " /";
+
+                permission = false;
+            }
+            else {
+                Debug.Log("choose a number first");
+            }
+        }
+        else
+        {
+            if (!permission)
+            {
+                mainString += " " + buttons[Int32.Parse(EventSystem.current.currentSelectedGameObject.tag)-1].text;
+                permission = true;
+            }
+            
+
+        }
+        Calculate();
+        Debug.Log(mainString);
 
     }
     public void Calculate()
     {
-     
+        mainString=mainString.Trim();
+       string [] array = mainString.Split(' ');
+        if (array.Length==3){
+            mainString = "";
+            switch (array[1])
+            {
+                case "+":mainString=""+(Int32.Parse(array[0]) + Int32.Parse(array[2]));
+                    break;
+                case "-":
+                    mainString = "" + (Int32.Parse(array[0]) - Int32.Parse(array[2]));
+                    break;
+                case "*":
+                    mainString = "" + (Int32.Parse(array[0]) * Int32.Parse(array[2]));
+                    break;
+
+                case "/":
+                    mainString = "" + (Int32.Parse(array[0]) /Int32.Parse(array[2]));
+                    break;
+
+            }
+        }
     }
+   
 }
